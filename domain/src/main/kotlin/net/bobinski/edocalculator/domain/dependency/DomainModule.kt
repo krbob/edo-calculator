b3c32758c5b6933e78e5a3ac8518a4798e0fca57
Krzysztof Bobiński <krzysztof@bobinski.net>
2025-10-31 16:32:00 +0100

    Add /inflation/cumulative endpoint

--- domain/src/main/kotlin/net/bobinski/edocalculator/domain/dependency/DomainModule.kt
+++ domain/src/main/kotlin/net/bobinski/edocalculator/domain/dependency/DomainModule.kt
@@ -1,6 +1,8 @@
 package net.bobinski.edocalculator.domain.dependency
 
+import net.bobinski.edocalculator.domain.usecase.CalculateCumulativeInflationUseCase
 import org.koin.dsl.module
 
 val DomainModule = module {
+    single { CalculateCumulativeInflationUseCase(inflationProvider = get(), currentTimeProvider = get()) }
 }
--- src/main/kotlin/net/bobinski/edocalculator/Routing.kt
+++ src/main/kotlin/net/bobinski/edocalculator/Routing.kt
@@ -3,11 +3,10 @@ package net.bobinski.edocalculator
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import net.bobinski.edocalculator.route.cumulativeInflationRoute
 
 fun Application.configureRouting() {
     routing {
-        get("/") {
-            call.respondText("Hello World!")
-        }
+        cumulativeInflationRoute()
     }
 }
